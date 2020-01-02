f6f48bd934f2aa55f47e4ef5e78dcd78131707fb
anmolkoirala <anmolkoirala3@gmail.com>
2020-01-02 18:04:39 +0545

    user registration API created

--- routers/userrouter.js
+++ routers/userrouter.js
@@ -4,6 +4,20 @@ const router = new express.Router;
 const bcrypt = require('bcrypt');
 const app = express();
 
-
+//user registration
+router.post("/user/registration",(req, res) => {
+    //changed values
+    req.body.password = bcrypt.hashSync(req.body.password, 10);
+    console.log(req.body);
+    user.create(req.body).then(function(){
+        return res.json({successmsg :"User Account Successfully created"});
+    }).catch(function(e){
+        if(e.name === "ValidationError"){
+            return res.status(403).json({errmsg: "Email already in use. Try again!"});
+        }else{
+            res.send(e);
+        }
+    });
+});
 
 module.exports = router;
