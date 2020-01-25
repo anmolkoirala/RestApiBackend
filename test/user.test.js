dfc93f6173d55be8ea6b5aac9876420c2f0f9ae5
anmolkoirala <anmolkoirala3@gmail.com>
2020-01-25 23:58:18 +0545

    test initial setup

---
+++ test/user.test.js
@@ -0,0 +1,23 @@
+const User = require('../models/user');
+const mongoose = require('mongoose');
+
+ 
+
+// use the new name of the database
+const url = 'mongodb://localhost:27017/groceryappDbTesting'; 
+beforeAll(async () => {
+    await mongoose.connect(url, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify:false
+    });
+});
+
+afterAll(async () => {
+
+    await mongoose.connection.close();
+});
+
+
