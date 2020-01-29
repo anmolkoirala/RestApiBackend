61cab8b6dabb055dbc5ab12412b560bfad6de144
anmolkoirala <anmolkoirala3@gmail.com>
2020-01-29 21:09:50 +0545

    add user cart test

--- test/cart.test.js
+++ test/cart.test.js
@@ -19,3 +19,30 @@ afterAll(async () => {
 
     await mongoose.connection.close();
 });
+
+
+describe('User Cart Schema testing', () => {
+    // the code below is for insert testing
+        it('Add item to cart', () => {
+            const usercart = {
+                'productid': '5e347cdbe0eafd2d4e6fda80',
+                'productname': 'Sweet Potato',
+                'productprice': '120',
+                'productcategory': 'vegetables',
+                'productimage': 'image-1580498139510.png',
+                'productnumber': 8,
+                'addedbyName': 'Obi Wan Kenobi',
+                'addedbyID': '5e2f14011fee7c13aded5409',
+            };
+
+
+
+            return UserCart.create(usercart)
+                .then((pro_ret) => {
+                    expect(pro_ret.productnumber).toEqual(8);
+                });
+        });
+
+
+
+})
